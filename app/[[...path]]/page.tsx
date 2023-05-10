56398494f93a467a5e46ec837a7bc0fd7e96aca2
Ash <ashley@juice49.com>
2023-05-10 23:59:25 +0100

    Revert "add edge runtime"
    This reverts commit 3fedc2cea7c61aa5c278b3d02b4ee091dcf54ad7.

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -27,7 +27,6 @@ export default async function Home({ params }: Props) {
 export const revalidate = 3600
 export const dynamic = 'force-static'
 export const fetchCache = 'only-cache'
-export const runtime = 'edge'
 
 // export function generateStaticParams() {
 //   return [
