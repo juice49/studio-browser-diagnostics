0024124f62beae91ca73183077fd442d94621d73
Ash <ashley@juice49.com>
2023-05-10 23:47:40 +0100

    Revert "add logging"
    This reverts commit 63520171864ee79b41162ef898b6653b545c6a62.

--- app/save-test-result.ts
+++ app/save-test-result.ts
@@ -8,20 +8,12 @@ export default async function saveTestResult(
   testId: string,
   result: TestSuiteResult,
 ) {
-  console.log('[PRE WRITE]', testId)
-
-  try {
-    await writeClient
-      .patch(testId)
-      .set({
-        result: JSON.stringify(result),
-      })
-      .commit()
-
-    console.log('[SUCCEEDED TO WRITE]')
-  } catch (error) {
-    console.log('[FAILED TO WRITE]', error)
-  }
+  await writeClient
+    .patch(testId)
+    .set({
+      result: JSON.stringify(result),
+    })
+    .commit()
 
   revalidatePath(testId)
 }
