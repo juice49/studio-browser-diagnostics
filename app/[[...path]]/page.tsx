268a6cbee684edefdc5355006ac4d37fe13a7b5d
Ash <ashley@juice49.com>
2023-05-10 23:49:20 +0100

    remove generateStaticParams

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -28,13 +28,13 @@ export const revalidate = 3600
 export const dynamic = 'force-static'
 export const fetchCache = 'only-cache'
 
-export function generateStaticParams() {
-  return [
-    {
-      path: ['5e2b94d4-856f-43fe-ab1b-761429387095'],
-    },
-  ]
-}
+// export function generateStaticParams() {
+//   return [
+//     {
+//       path: ['5e2b94d4-856f-43fe-ab1b-761429387095'],
+//     },
+//   ]
+// }
 
 interface TestData {
   user: {
