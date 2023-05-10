470c7b2de751d96e4a9bea7616b00251986520df
Ash <ashley@juice49.com>
2023-05-10 23:55:25 +0100

    add edge runtime

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -27,6 +27,7 @@ export default async function Home({ params }: Props) {
 export const revalidate = 3600
 export const dynamic = 'force-static'
 export const fetchCache = 'only-cache'
+export const runtime = 'edge'
 
 // export function generateStaticParams() {
 //   return [
